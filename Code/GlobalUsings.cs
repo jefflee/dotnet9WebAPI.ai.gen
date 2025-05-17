b57bcc710d1424cacfd56df06fe1cd3e08040118
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2025-05-17 16:14:42 +0800

    Remove project.nuget.cache file to clean up unnecessary cache data

---
+++ Code/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives for common namespaces
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
